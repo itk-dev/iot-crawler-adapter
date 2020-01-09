9aa3278655fb4b9a30cb65cc9616c67315a061af
Mikkel Ricky <rimi@aarhus.dk>
2020-01-09 10:30:59 +0100

    Cleaned up error messages

--- src/Controller/LoriotController.php
+++ src/Controller/LoriotController.php
@@ -11,6 +11,7 @@
 namespace App\Controller;
 
 use App\Loriot\DataManager;
+use Exception;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -28,33 +29,41 @@ class LoriotController
      */
     public function post(Request $request, SerializerInterface $serializer, DataManager $dataManager)
     {
-        $dataFormat = $request->get('dataFormat');
-        if (empty($dataFormat)) {
-            return $this->badRequest(sprintf('Missing data format: %s', $dataFormat));
-        }
-
-        $content = $request->getContent();
-        $contentType = $request->headers->get('content-type');
-
-        if (0 === strpos($contentType, 'application/json')) {
-            try {
-                $payload = $serializer->decode($content, 'json');
-
-                $dataManager->handle($payload, $dataFormat);
-
-                return new JsonResponse('OK');
-            } catch (UnexpectedValueException $exception) {
-                return $this->badRequest(sprintf('Invalid request: %s', $exception->getMessage()));
+        try {
+            $dataFormat = $request->get('dataFormat');
+            if (empty($dataFormat)) {
+                return $this->badRequest(sprintf('Missing data format: %s', $dataFormat));
             }
-        } else {
-            return $this->badRequest(sprintf('Invalid content-type: %s', $contentType));
+
+            $content = $request->getContent();
+            $contentType = $request->headers->get('content-type');
+
+            if (0 === strpos($contentType, 'application/json')) {
+                try {
+                    $payload = $serializer->decode($content, 'json');
+
+                    $dataManager->handle($payload, $dataFormat);
+
+                    return new JsonResponse('OK');
+                } catch (UnexpectedValueException $exception) {
+                    return $this->badRequest($exception->getMessage());
+                }
+            } else {
+                return $this->badRequest(sprintf('Invalid content-type: %s', $contentType));
+            }
+        } catch (Exception $exception) {
+            return $this->badRequest($exception->getMessage());
         }
 
         return $this->badRequest();
     }
 
-    private function badRequest(string $message = 'Invalid request'): JsonResponse
+    private function badRequest(string $message = 'Invalid request', string $details = null): JsonResponse
     {
-        return new JsonResponse(['message' => $message], Response::HTTP_BAD_REQUEST);
+        // @see https://tools.ietf.org/html/rfc7807#page-3
+        return new JsonResponse(array_filter([
+            'title' => $message,
+            'details' => $details,
+        ]), Response::HTTP_BAD_REQUEST);
     }
 }
