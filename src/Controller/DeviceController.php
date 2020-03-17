702f4dce9b6a6c80b6e02b0b8eddc86ed62536cd
Mikkel Ricky <rimi@aarhus.dk>
2020-03-17 09:51:36 +0100

    Added links in responses

--- src/Controller/DeviceController.php
+++ src/Controller/DeviceController.php
@@ -10,10 +10,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Device;
 use App\Repository\DeviceRepository;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
 
 /**
  * @Route("/devices", name="device_")
@@ -23,10 +25,21 @@ class DeviceController extends ApiController
     /**
      * @Route("", name="index")
      */
-    public function index(DeviceRepository $repository, SerializerInterface $serializer): Response
+    public function index(DeviceRepository $repository, NormalizerInterface $normalizer): Response
     {
         $devices = $repository->findAll();
+        $data = $normalizer->normalize($devices, 'json', ['groups' => ['device', 'links']]);
 
-        return $this->createJsonResponse($devices, ['groups' => 'device']);
+        return new JsonResponse($data);
+    }
+
+    /**
+     * @Route("/{device}", name="show")
+     */
+    public function show(Device $device, NormalizerInterface $normalizer): Response
+    {
+        $data = $normalizer->normalize($device, 'json', ['groups' => ['device', 'links']]);
+
+        return new JsonResponse($data);
     }
 }
---
+++ src/Serializer/DeviceNormalizer.php
@@ -0,0 +1,57 @@
+<?php
+
+/*
+ * This file is part of itk-dev/iot-crawler-adapter.
+ *
+ * (c) 2020 ITK Development
+ *
+ * This source file is subject to the MIT license.
+ */
+
+namespace App\Serializer;
+
+use App\Entity\Device;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Serializer\Normalizer\ContextAwareNormalizerInterface;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+
+class DeviceNormalizer implements ContextAwareNormalizerInterface
+{
+    private $router;
+    private $normalizer;
+
+    public function __construct(UrlGeneratorInterface $router, ObjectNormalizer $normalizer)
+    {
+        $this->router = $router;
+        $this->normalizer = $normalizer;
+    }
+
+    public function normalize($device, string $format = null, array $context = [])
+    {
+        $data = $this->normalizer->normalize($device, $format, $context);
+
+        if (\in_array('links', $context['groups'] ?? [], true)) {
+            $data['links'] = [
+                'self' => $this->router->generate('device_show', [
+                    'device' => $device->getId(),
+                ], UrlGeneratorInterface::ABSOLUTE_URL),
+            ];
+
+            foreach ($data['sensors'] as &$sensor) {
+                $sensor['links'] = [
+                    'latest_measurement' => $this->router->generate('measurement_latest', [
+                        'device' => $device->getId(),
+                        'sensor' => $sensor['id'],
+                    ], UrlGeneratorInterface::ABSOLUTE_URL),
+                ];
+            }
+        }
+
+        return $data;
+    }
+
+    public function supportsNormalization($data, string $format = null, array $context = [])
+    {
+        return $data instanceof Device;
+    }
+}
